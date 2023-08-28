f3804c87adcec263aa1614ceb1f59e54061510fc
tuyizereerneste <tuyizereernest@yahoo.com>
2023-08-29 00:58:55 +0200

    Safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             result += 1
         except IndexError:
             break
-        print("")
-        return (result)
+    print("")
+    return (result)
