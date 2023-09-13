2f346d19e90c4325d20d9b00aa1e616b9a391c4f
tuyizereerneste <tuyizereernest@yahoo.com>
2023-09-13 19:29:45 +0200

    Print alphabets altenation lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,6 +5,6 @@ not followed by a new line.
 """
 for letter in range(122, 96, -1):
     if letter % 2 != 0:
-        print("{}".format(chr(letter -32)), end="")
+        print("{}".format(chr(letter - 32)), end="")
     else:
         print("{}".format(chr(letter)), end="")
