9cec5d570c321c1087bd941b88f7394df3ea8eb2
tuyizereerneste <tuyizereernest@yahoo.com>
2023-09-22 15:07:52 +0200

    assigns attributes arguments of square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,6 +30,28 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.width)
 
+    def update(self, *args, **kwargs):
+        """Update attributes of square"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
         return {
