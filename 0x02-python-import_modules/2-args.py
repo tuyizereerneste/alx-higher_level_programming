5e13261aba86f611b07f38d42646cd48cb67b3df
tuyizereerneste <tuyizereernest@yahoo.com>
2023-10-02 15:48:27 +0200

    print number and arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+
+    args = len(sys.argv) - 1
+    if (args == 0):
+        print("0 argument.")
+    elif (args == 1):
+        print("1 argument:")
+    else:
+        print("{} argument:".format(args))
+
+    for i in range(args):
+        print("{}: {}".format(i + 1, (sys.argv[i + 1])))
