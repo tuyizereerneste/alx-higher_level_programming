d89b5ee0e8783d59800c351cd8e5be540573b992
tuyizereerneste <tuyizereernest@yahoo.com>
2023-10-18 02:17:48 +0300

    updates the score of Bob to 10

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
---Update the score of Bob to 10 in the table second_table in my MySQL server.
+-- Update the score of Bob to 10 in the table second_table in my MySQL server.
 UPDATE `second_table`
 SET `score` = 10
 WHERE `name` = "Bob";
