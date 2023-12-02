ccc0e92ec0283246759493f844fa1f4d5a4d5247
tuyizereerneste <tuyizereernest@yahoo.com>
2023-12-02 02:43:21 +0200

    Revert "access to the GitHub API update"
    This reverts commit 52a30e4bc7d9aa7611319bd5993d6ff87b37ea9e.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -18,24 +18,24 @@ def func(argv):
 
     def print_commits(i, commit_list):
         """function that prints 10 commits"""
-        sha = commit_list[i].get('sha')
-        commit = commit_list[i].get('commit')
-        author = commit.get('author')
+        var = commit_list[i].get('var')
+        com = commit_list[i].get('com')
+        author = com.get('author')
         name = author.get('name')
-        print('{}: {}'.format(sha, name))
-    repo = argv[1]
+        print('{}: {}'.format(var, name))
+    repository = argv[1]
     owner = argv[2]
     headers = {"Accept": "application/vnd.github.v3+json"}
     response = requests.get('https://api.github.com/repos/' + owner +
-                            '/' + repo + '/commits', headers=headers)
+                            '/' + repository + '/commits', headers=headers)
     commit_list = response.json()
     size = len(commit_list)
     if size < 10:
-        for i in range(0, size):
-            print_commits(i, commit_list)
+        for c in range(0, size):
+            print_commits(c, commit_list)
         else:
-            for i in range(0, 10):
-                print_commits(i, commit_list)
+            for c in range(0, 10):
+                print_commits(c, commit_list)
 
 
 if __name__ == "__main__":
