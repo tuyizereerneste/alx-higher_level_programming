49ab3c3f1664755dff1c1bab6bd1e5ce82804466
tuyizereerneste <tuyizereernest@yahoo.com>
2024-01-03 19:37:22 +0200

    read in file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf-8', function (error, data) {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     console.log(data);
   }
