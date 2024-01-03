ed7d5e5a35a84fa634e10c7fa91522b36b77add6
tuyizereerneste <tuyizereernest@yahoo.com>
2024-01-03 23:58:07 +0200

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request.get(url).on('response', function (response) {
-  console.log('Code: ', response.statusCode);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log('Code: ${response.statusCode}');
 });
